5ae807c599f6fe62b3583b05217b34402e736b3d
TienNguyen <ngoctiennguyen155@gmail.com>
2020-08-04 17:56:56 +0700

    model

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+     // Start is called before the first frame update
+
+     public float speed;
+     private float vertical, horizontal;
+
+     public Animator ani;
+     private Quaternion rot;
+     void Start()
+     {
+          //rot = transform.rotation;
+          ani.SetBool("isMove", true);
+     }
+
+     // Update is called once per frame
+     void Update()
+     {
+          ani.SetFloat("hor", Input.GetAxis("Horizontal"));
+          ani.SetFloat("ver", Input.GetAxis("Vertical"));
+
+          //   Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+          //   RaycastHit hit;
+          //   if (Physics.Raycast(ray, out hit))
+          //   {
+          //        transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
+          //   }
+     }
+     void FixedUpdate()
+     {
+
+     }
+     void OnTriggerEnter(Collider other)
+     {
+          if (other.CompareTag("wall"))
+          {
+
+
+          }
+
+     }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+     // Start is called before the first frame update
+     public float speed;
+     private float vertical, horizontal;
+     public Rigidbody rb;
+
+     void Start()
+     {
+     }
+
+     // Update is called once per frame
+     void Update()
+     {
+          vertical = Input.GetAxis("Vertical");
+          horizontal = Input.GetAxis("Horizontal");
+          Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+          RaycastHit hit;
+          if (Physics.Raycast(ray, out hit))
+          {
+               transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
+          }
+     }
+     void FixedUpdate()
+     {
+          rb.MovePosition(rb.position + new Vector3(horizontal, 0f, vertical) * speed * Time.deltaTime);
+          if (Input.GetKeyDown(KeyCode.Space))
+          {
+               rb.MovePosition(rb.position + new Vector3(0f, 5f, 0f) * speed * Time.deltaTime);
+          }
+     }
+}
